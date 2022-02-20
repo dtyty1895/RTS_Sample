96e9ca3d3d1183b33937f3e2419d3366e74f31e4
Tai <d.tyty1895@gmail.com>
2022-02-20 16:47:29 +0800

    first commit

---
+++ How-To-Build-An-RTS-Series-master/Assets/_Scripts/Interactables/IBuilding.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace LP.FDG.Interactables
+{
+    public class IBuilding : Interactable
+    {
+        public UI.HUD.PlayerActions actions;
+
+        public override void OnInteractEnter()
+        {
+            UI.HUD.ActionFrame.instance.SetActionButtons(actions, gameObject);
+            base.OnInteractEnter();
+        }
+
+        public override void OnInteractExit()
+        {
+            UI.HUD.ActionFrame.instance.ClearActions();
+            base.OnInteractExit();
+        }
+    }
+}
+
